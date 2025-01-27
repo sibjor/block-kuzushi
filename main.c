7a67842d83b4acbddc7287250d209a2792577611
sibjor <sixteen610@gmail.com>
2025-01-27 18:34:33 +0100

    Added bouncing ball without collision

--- main.c
+++ main.c
@@ -2,24 +2,48 @@
 #include "raymath.h"
 
 typedef struct Player {
-    Vector2 position;
-    float radius;
+    int posX;
+    int posY;
+    int width;
+    int height;
 } Player;
+typedef struct Ball{
+  int posX;
+  int posY;
+  float radius;
+  float speedX;
+  float speedY;
+}Ball;
+
+void BallBounce(Ball *ball){
+  if(ball->posX <= 0 || ball->posX>= 800){
+    ball->speedX *= -1;
+  }
+  if(ball->posY<= 0 || ball->posY >= 800){
+    ball->speedY *= -1;
+  }
+}
 
 int main() {
     InitWindow(800, 800, "Block-Kuzushi");
     SetTargetFPS(60);
 
-    Player player = { {400, 750}, 20.0f };
+    Player player = { 400, 750, 100, 20};
+    Ball ball = {400, 400, 16.0f, 5.0f, 5.0f};
 
     while (!WindowShouldClose()) {
         // Update player position
-        if (IsKeyDown(KEY_RIGHT)) player.position.x += 5.0f;
-        if (IsKeyDown(KEY_LEFT)) player.position.x -= 5.0f;
+        if (IsKeyDown(KEY_RIGHT)) player.posX += 5.0f;
+        if (IsKeyDown(KEY_LEFT)) player.posX -= 5.0f;
+        // Update ball position
+        ball.posX += ball.speedX;
+        ball.posY += ball.speedY;
+        BallBounce(&ball);
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawCircleV(player.position, player.radius, BLACK);
+        DrawRectangle(player.posX, player.posY,player.width, player.height, BLACK);
+        DrawCircle(ball.posX, ball.posY, ball.radius, BLACK);
         EndDrawing();
     }
 
