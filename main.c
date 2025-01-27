bdb25fce69053f77dce9da3a4c9568d0e9f454c4
sibjor <sixteen610@gmail.com>
2025-01-27 14:35:33 +0100

    A basic window

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include "raylib.h"
+#include "raymath.h"
 
 int main() {
-  
+    InitWindow(800, 800, "Block-Kuzushi");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
