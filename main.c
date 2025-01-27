81783c8c302d9d98cc5a87f9a2f4901148e968f6
sibjor <sixteen610@gmail.com>
2025-01-27 14:51:18 +0100

    Added player

--- main.c
+++ main.c
@@ -1,13 +1,25 @@
 #include "raylib.h"
 #include "raymath.h"
 
+typedef struct Player {
+    Vector2 position;
+    float radius;
+} Player;
+
 int main() {
     InitWindow(800, 800, "Block-Kuzushi");
     SetTargetFPS(60);
 
+    Player player = { {400, 750}, 20.0f };
+
     while (!WindowShouldClose()) {
+        // Update player position
+        if (IsKeyDown(KEY_RIGHT)) player.position.x += 5.0f;
+        if (IsKeyDown(KEY_LEFT)) player.position.x -= 5.0f;
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        DrawCircleV(player.position, player.radius, BLACK);
         EndDrawing();
     }
 
